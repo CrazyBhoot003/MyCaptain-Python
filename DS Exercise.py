eaeb31b4c0845dae41f753e6e8c7b5d44218b933
ak01234 <53751299+ak01234@users.noreply.github.com>
2020-12-15 14:21:17 +0530

    Add files via upload
    This Python File contains the Solution of the DS Exercises. 
    
    1. Area of the Circle
    2. Getting the File Extension

---
+++ DS Exercise.py
@@ -0,0 +1,12 @@
+#Area of the Circle
+rad = float(input("Input the radius of the circle:"))
+area = 3.14*rad*rad
+
+print("The area of the circle with radius:",rad,"is:",area)
+
+#Printing the File Extension
+file = input("Input the Filename:")
+file = file.split(".")
+
+print("The extension of the file is:",file[1])
+
